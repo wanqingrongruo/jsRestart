301cec03fa86f2d385545593989ee94b50e574a6
roni <1210538084@qq.com>
2017-09-08 15:55:09 +0800

    错误验证

---
+++ demo02/validated.js
@@ -0,0 +1,34 @@
+function validate_required(field, alerttxt) {
+
+    with(field) {
+        if (value == null || value == "") {
+            alert(alerttxt)
+            return false
+        } else {
+            return true
+        }
+    }
+}
+
+function validate_eamil(field, alerttxt) {
+    with(field) {
+
+        apos = value.indexOf("@")
+        dotpos = value.lastIndexOf(".")
+        if (apos < 1 || dotpos < 2) {
+            alert(alerttxt)
+            return false
+        } else {
+            return true
+        }
+    }
+}
+
+function validate_form(thisform) {
+    with(thisform) {
+        if (validate_required(email, "Email must be filled out!" == false)) {
+            email.focus()
+            return false
+        }
+    }
+}

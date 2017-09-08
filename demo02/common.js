1c86d08b0be903b01d16f57fd5138280c9ebc7cb
roni <1210538084@qq.com>
2017-09-08 15:12:37 +0800

    demo01 and demo02

---
+++ demo02/common.js
@@ -0,0 +1,29 @@
+function showInfo() {
+
+    // var arr = new Array()
+    // arr[0] = "roni"
+    // arr[1] = "dev"
+    // arr = new Array(
+    //     "11",
+    //     "2"
+    // )
+    // arr = ["1", "2"]
+
+    // var dic = { name: "roni", sex: "man", age = 25 }
+    // var name = dic.name
+
+    // name = undefined
+    // name = null
+
+    // //JavaScript 变量均为对象。当您声明一个变量时，就创建了一个新的对象
+    // var person = new Object
+    // var s = new String
+
+    // //如果您把值赋给尚未声明的变量，该变量将被自动作为全局变量声明
+    // work = "iOS"
+
+    // 如果把数字与字符串相加，结果将成为字符串
+    x = "5" + 5
+    document.write(x)
+        // document.getElementById("demo").innerHTML = "js out"
+}
